075ec4e3f35fa07983f69d622eb2b697938001a4
khaingthazindev <khaingthazin.dev@gmail.com>
2026-04-22 11:10:22 +0630

    create first functional component

---
+++ src/app/components/Profile.jsx
@@ -0,0 +1,12 @@
+export default function Profile() {
+		return (<div>
+				<h3>Profile</h3>
+				<img
+						src="https://react.dev/images/docs/scientists/MK3eW3Am.jpg"
+						alt="Katherine Johnson"
+						width={"100"}
+						height={"100"}
+						className={"profile"}
+				/>
+		</div>);
+}
